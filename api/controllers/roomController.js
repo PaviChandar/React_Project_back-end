e50082215d258afe538f7a6f4ff3d76e40c6cdb7
PaviChandar <pavithrachandar0501@gmail.com>
2022-09-12 16:00:27 +0530

    imp changes

--- api/controllers/roomController.js
+++ api/controllers/roomController.js
@@ -18,6 +18,7 @@ export const createRoom = async (req, res, next) => {
         }
         res.status(200).json(savedRoom)
     } catch (err) {
+        console.log("error : ",err)
         next(err)
     }
 }
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -1,12 +1,11 @@
 import jwt from "jsonwebtoken";
 import   createError  from "../utils/error.js"
+import jwtDecode from "jwt-decode";
 
 //verifyToken - middleware
 export const verifyToken = (req,res,next) => {
     // const token = req.cookies.access_token;
-    const token = req.headers.authorization.split(' ')[1]
-    console.log("token",token)
-    console.log("jwt",process.env.JWT)
+    const token = req.headers.authorization.split(' ')[1].split('\"')[1]
     //if not a token
     if(!token) {
         return next(createError(401, "You are not authenticated"))
@@ -14,7 +13,10 @@ export const verifyToken = (req,res,next) => {
 
     //if no error and if a token, verify if it is a correct one
     jwt.verify(token, process.env.JWT, (err, user) => {
+
+
         if(err){
+            console.log('error : ',err)
         return next(createError(403, "Token is not valid!"))
         }
 
@@ -34,9 +36,11 @@ export const verifyUser = (req,res,next) => {
 }
 
 export const verifyAdmin = (req,res,next) => {
+    const { isAdmin } = jwtDecode(req.headers['authorization'].split(' ')[1])
     verifyToken(req,res,next, () => {
-        if(req.user.isAdmin){ //only owner of acc and admin can delete the acc
+        if(isAdmin){ //only owner of acc and admin can delete the acc
         next();
+        console.log('called')
         } else {
         return next(createError(403,"You are not authorized"))
  } });
